a6b86df66fbaa9035890963408f0711be85f4736
SnBkSpy <rhickson94@gmail.com>
2025-06-16 00:56:22 +0100

    Basic code starters

---
+++ cardDecks.py
@@ -0,0 +1,31 @@
+cottageDeck = ["cottage"]
+
+farmDeck = ["farm", "orchard", "greenhouse", "granary"]
+
+factoryDeck = ["factory", "warehouse", "tradingPost", "bank"]
+
+tavernDeck = ["tavern", "inn", "almshouse", "feastHall"]
+
+chapelDeck = ["chapel", "temple", "abbey", "cloister"]
+
+theatreDeck = ["theatre", "bakery", "market", "tailor"]
+
+wellDeck = ["well", "fountain", "millstone", "shed"]
+
+monumentsDeck = [
+    "architectsGuild",
+    "archiveOfTheSecondAge",
+    "barettCastle",
+    "cathedralOfCaterina",
+    "fortIronweed",
+    "grandMausoleumOfTheRodina",
+    "groveUniversity",
+    "mandrasPalace",
+    "obeliskOfTheCrescent",
+    "opaleyesWatch",
+    "shrineOfTheElderTree",
+    "silvaForum",
+    "theSkyBaths",
+    "theStarloom",
+    "statueOfTheBondmaker"
+]
---
+++ resources.py
@@ -0,0 +1,9 @@
+# RESOURCES
+
+
+class resource:
+    """
+    A class to define the resources used in the game to construct buildings within the town boards
+    """
+    resourceTypes = ["wood", "wheat", "glass", "brick", "stone"]
+    #                   1       2       3           4       5
