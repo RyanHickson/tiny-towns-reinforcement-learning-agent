156d8ee804e4bf5ab28e29b1af24f390d32a70af
Ryan <rhickson94@gmail.com>
2025-06-23 18:44:27 +0100

    Town board now contains class value not string

--- building_layouts.py
+++ building_layouts.py
@@ -1,6 +1,7 @@
 import numpy as np
 from layout_variants import *
 from resources import *
+from cards import *
 
 # W = WOOD
 # C = WHEAT (CROP)
@@ -9,127 +10,127 @@ from resources import *
 # S = STONE
 
 # COTTAGE TYPES
-cottage_layout = [[" ","wheat"],
-                 ["brick","glass"]]
+cottage_layout = [[wild,wheat],
+                  [brick,glass]]
 
 # FARM TYPES
-farm_layout = [["wheat","wheat"],
-              ["wood","wood"]]
+farm_layout = [[wheat,wheat],
+               [wood,wood]]
 
-orchard_layout = [["stone","wheat"],
-                 ["wheat","wood"]]
+orchard_layout = [[stone,wheat],
+                 [wheat,wood]]
 
-greenhouse_layout = [["wheat","glass"],
-                    ["wood","wood"]]
+greenhouse_layout = [[wheat,glass],
+                     [wood,wood]]
 
-granary_layout = [["wheat","wheat"],
-                 ["wood","brick"]]
+granary_layout = [[wheat,wheat],
+                  [wood,brick]]
 
 # FACTORY TYPES
-factory_layout = [["wood"," "," "," "],
-                 ["brick","stone","stone","brick"]]
+factory_layout = [[wood,wild,wild,wild],
+                  [brick,stone,stone,brick]]
 
-warehouse_layout = [["wheat","wood","wheat"],
-                   ["brick"," ","brick"]]
+warehouse_layout = [[wheat,wood,wheat],
+                    [brick,wild,brick]]
 
-trading_post_layout = [["stone","wood"," "],
-                     ["stone","wood","brick"]]
+trading_post_layout = [[stone,wood,wild],
+                       [stone,wood,brick]]
 
-bank_layout = [["wheat","wheat"," "],
-              ["wood","glass","brick"]]
+bank_layout = [[wheat,wheat,wild],
+               [wood,glass,brick]]
 
 # TAVERN TYPES
-tavern_layout = [["brick","brick","glass"]]
+tavern_layout = [[brick,brick,glass]]
 
-almshouse_layout = [["stone","stone","glass"]]
+almshouse_layout = [[stone,stone,glass]]
 
-inn_layout = [["wheat","stone","glass"]]
+inn_layout = [[wheat,stone,glass]]
 
-feast_hall_layout = [["wood","wood","glass"]]
+feast_hall_layout = [[wood,wood,glass]]
 
 # CHAPEL TYPES
-chapel_layout = [[" "," ","glass"],
-                ["stone","glass","stone"]]
+chapel_layout = [[wild,wild,glass],
+                 [stone,glass,stone]]
 
-temple_layout = [[" "," ","glass"],
-                ["brick","brick","stone"]]
+temple_layout = [[wild,wild,glass],
+                 [brick,brick,stone]]
 
-abbey_layout = [[" "," ","glass"],
-               ["brick","stone","stone",]]
+abbey_layout = [[wild,wild,glass],
+                [brick,stone,stone,]]
 
-cloister_layout = [[" "," ","glass"],
-                  ["wood","brick","stone"]]
+cloister_layout = [[wild,wild,glass],
+                   [wood,brick,stone]]
 
 # THEATRE TYPES
-theatre_layout = [[" ","stone"," "],
-                 ["wood","glass","wood"]]
+theatre_layout = [[wild,stone,wild],
+                  [wood,glass,wood]]
 
-tailor_layout = [[" ","wheat"," "],
-                ["stone","glass","stone"]]
+tailor_layout = [[wild,wheat,wild],
+                 [stone,glass,stone]]
 
-market_layout = [[" ","wood"," "],
-                ["stone","glass","stone"]]
+market_layout = [[wild,wood,wild],
+                 [stone,glass,stone]]
 
-bakery_layout = [[" ","wheat"," "],
-                ["brick","glass","brick"]]
+bakery_layout = [[wild,wheat,wild],
+                 [brick,glass,brick]]
 
 # WELL TYPES
-well_layout = [["wood","stone"]]
+well_layout = [[wood,stone]]
 
-fountain_layout = [["wood","stone"]]
+fountain_layout = [[wood,stone]]
 
-millstone_layout = [["wood","stone"]]
+millstone_layout = [[wood,stone]]
 
-shed_layout = [["wood","stone"]]
+shed_layout = [[wood,stone]]
 
 # MONUMENTS
-architects_guild_layout = [[" "," ","glass"],
-                         [" ","wheat","stone"],
-                         ["wood","brick"," "]]
+architects_guild_layout = [[wild,wild,glass],
+                           [wild,wheat,stone],
+                           [wood,brick,wild]]
 
-archive_of_the_second_age_layout = [["wheat","wheat"],
-                               ["brick","glass"]]
+archive_of_the_second_age_layout = [[wheat,wheat],
+                                    [brick,glass]]
 
-barrett_castle_layout = [["wheat"," "," ","stone"],
-                       ["wood","glass","glass","brick"]]
+barrett_castle_layout = [[wheat,wild,wild,stone],
+                         [wood,glass,glass,brick]]
 
-cathedral_of_caterina_layout = [[" ","wheat"],
-                             ["stone","glass"]]
+cathedral_of_caterina_layout = [[wild,wheat],
+                                [stone,glass]]
 
-fort_ironweed_layout = [["wheat"," ","brick"],
-                      ["stone","wood","stone"]]
+fort_ironweed_layout = [[wheat,wild,brick],
+                        [stone,wood,stone]]
 
-grand_mausoleum_of_the_rodina_layout = [["wheat","wheat"],
-                                   ["brick","stone"]]
+grand_mausoleum_of_the_rodina_layout = [[wheat,wheat],
+                                        [brick,stone]]
 
-grove_university_layout = [[" ","brick"," "],
-                         ["stone","glass","stone"]]
+grove_university_layout = [[wild,brick,wild],
+                           [stone,glass,stone]]
 
-mandras_palace_layout = [["wheat","glass"],
-                       ["brick","wood"]]
+mandras_palace_layout = [[wheat,glass],
+                         [brick,wood]]
 
-obelisk_of_the_crescent_layout = [["wheat"," "," "],
-                              ["brick","glass","brick"]]
+obelisk_of_the_crescent_layout = [[wheat,wild,wild],
+                                  [brick,glass,brick]]
 
-opaleyes_watch_layout = [["wood"," "," "," "],
-                       ["brick","glass","wheat","wheat"],
-                       ["stone"," "," "," "]]
+opaleyes_watch_layout = [[wood,wild,wild,wild],
+                         [brick,glass,wheat,wheat],
+                         [stone,wild,wild,wild]]
 
-shrine_of_the_elder_tree_layout = [["brick","wheat","stone"],
-                              ["wood","glass","wood"]]
+shrine_of_the_elder_tree_layout = [[brick,wheat,stone],
+                                   [wood,glass,wood]]
 
-silva_forum_layout = [[" "," ","wheat"," "],
-                    ["brick","brick","stone","wood"]]
+silva_forum_layout = [[wild,wild,wheat,wild],
+                      [brick,brick,stone,wood]]
 
-the_sky_baths_layout = [[" ","wheat"," "],
-                     ["stone","glass","wood"],
-                     ["brick"," ","brick"]]
+the_sky_baths_layout = [[wild,wheat,wild],
+                        [stone,glass,wood],
+                        [brick,wild,brick]]
 
-the_starloom_layout = [["glass","glass",],
-                     ["wood","wheat"]]
+the_starloom_layout = [[glass,glass,],
+                       [wood,wheat]]
 
-statue_of_the_bondmaker_layout = [["wood","stone","stone","glass"],
-                                  ["wheat"," "," "," "]]
+statue_of_the_bondmaker_layout = [[wood,stone,stone,glass],
+                                  [wheat,wild,wild,wild]]
 
 def get_not_wilds(layout):
     """
@@ -138,7 +139,7 @@ def get_not_wilds(layout):
     co_ords = []
     for row_index, row in enumerate(layout):
         for col_index, cell in enumerate(row):
-            if cell != " ":
+            if cell != wild:
                 co_ords.append((row_index, col_index))
     return co_ords
 
@@ -164,7 +165,7 @@ def find_placements(board, card):
                 for r, c in not_wilds:
                     board_value = board[i + r][j + c]
                     layout_value = variant[r][c]
-                    if board_value != layout_value and board_value != wild:      # CHECK FOR RESOURCE MISMATCH AND WILD RESOURCE
+                    if board_value != layout_value.get_name() and board_value != wild:      # CHECK FOR RESOURCE MISMATCH AND WILD RESOURCE
                         match = False
                         break
                 if match:
